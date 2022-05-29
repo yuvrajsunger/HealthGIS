7b255047150a79e829cb6a9d45d6787ea4d5d1f7
yuvraj sunger <yuvrajsunger@gmail.com>
2022-05-29 20:37:33 +0530

    bootstrap styling

--- script/mapscript.js
+++ script/mapscript.js
@@ -1,4 +1,4 @@
-var map = L.map("map").setView([28.66,77.16], 11);
+var map = L.map("map").setView([28.66,77.10], 10.5);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
